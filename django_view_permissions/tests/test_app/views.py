5d6b0c0da73696a5a563f4b7d8e34ef27367d7e9
Ayub-khan <muhammadayubkhan6@gmail.com>
2020-03-02 23:04:59 +0500

    Basic Logic -Added permissions functionality -Updated Tests -Updated Requirements -Minor Nits

--- django_view_permissions/tests/test_app/views.py
+++ django_view_permissions/tests/test_app/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
+""" Test App views Module """
+from django_view_permissions.tests.test_app import BaseTestView
 
 
-def test(request):
-    """ Placeholder test view """
-    return HttpResponse("ok")
+class TestView(BaseTestView):
+    """
+    Test View
+    Initializing this with empty permissions
+    so that each test case could add its own
+    permissions.
+
+    Example :
+
+    permissions = [
+        ('method', permissions.have_permission),
+        ('attr', 'is_staff', True),
+        ('class', permissions.BasicTestPermissions)
+    ]
+
+    """
+    permissions = []
+
+
+test_view = TestView.as_view()  # pylint: disable=invalid-name
