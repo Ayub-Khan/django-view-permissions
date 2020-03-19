b5f1000ccd00e4d61a25c481176a38654d9a3237
Ayub-khan <muhammadayubkhan6@gmail.com>
2020-03-19 16:01:37 +0500

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Ayub khan'
 author = 'Ayub khan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
